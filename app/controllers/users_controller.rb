61f52224171f90aa5e91934803cdec52077762df
KosukeKawano <KosukeKawano@takehanKosuke.local>
2019-05-04 02:01:02 +0900

    tmp

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
     redirect_to root_url and return unless @user.activate
   end
 
@@ -55,13 +56,7 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
+
 
     def correct_user
       @user = User.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
+  resources :microposts,          only: [:create, :destroy]
 
   get '/home', to:'static_pages#home'
   get '/help', to:'static_pages#help'
