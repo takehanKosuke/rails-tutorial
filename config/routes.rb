0c7adefdc919ba35ef04a889c2360b91af88b4eb
KosukeKawano <KosukeKawano@takehanKosuke.local>
2019-05-03 23:42:08 +0900

    12章

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   get '/home', to:'static_pages#home'
   get '/help', to:'static_pages#help'
